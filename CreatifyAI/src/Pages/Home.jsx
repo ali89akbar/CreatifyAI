ce52d7a636c87cb99524ea4b12996ba7d9ad8d1d
ali89akbar <alia42268@gmail.com>
2025-08-24 21:32:58 +0500

    Footer Section Implemented

--- CreatifyAI/src/Pages/Home.jsx
+++ CreatifyAI/src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from '../Components/Hero';
 import AITools from '../Components/AITools';
 import Testimonials from '../Components/Testimonials';
 import Plan from '../Components/Plan';
+import Footer from '../Components/Footer';
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
     <AITools/>
     <Testimonials/>
     <Plan/>
+            <Footer/>
     </>
   )
 }
