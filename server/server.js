3103b3b31305a43aceff4a66706a299911850e46
ali89akbar <alia42268@gmail.com>
2025-08-28 02:22:29 +0500

    Server initialized

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express, { response } from "express";
+import cors from "cors";
+import 'dotenv/config';
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+app.get('/',(req,res)=>response.send('Server is Live!'))
+
+app.listen(process.env.PORT || 3000, () => console.log(`Server is running on port ${process.env.PORT || 3000}`));
