1b0309f520a9f37689a1ab3a9d74195112cc4807
ali89akbar <alia42268@gmail.com>
2025-08-28 02:50:52 +0500

    Setup Neon setup & Clerk setup & complete server structure

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 import express, { response } from "express";
 import cors from "cors";
 import 'dotenv/config';
+import {clerkMiddleware, requireAuth} from '@clerk/express';
 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json())
+app.use(clerkMiddleware());
+app.use(requireAuth());
+
 
 app.get('/',(req,res)=>response.send('Server is Live!'))
+
 
 app.listen(process.env.PORT || 3000, () => console.log(`Server is running on port ${process.env.PORT || 3000}`));
