562a788535c1e2939a01d51990671004c84614c9
ali89akbar <alia42268@gmail.com>
2025-08-27 21:18:26 +0500

    Remove Background screen Implemented and generate image add section

--- CreatifyAI/src/Pages/GenerateImages.jsx
+++ CreatifyAI/src/Pages/GenerateImages.jsx
@@ -24,10 +24,26 @@ function GenerateImage() {
        <p className='mt-6 text-sm font-medium'>Style</p>
        <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>{imageStyle.map((item)=>(
         <span onClick={()=> setSelectedStyle(item)} 
-        className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedStyle === item ? 'bg-purple-50 text-purple-500' : 'text-gray-300 border-gray-300 '}`} key={item}>{item}</span>
+        className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedStyle === item ? 'bg-purple-50 text-green-500 pt-1.5' : 'text-gray-300 border-gray-300 pt-1.5'}`} key={item}>{item}</span>
        ))}
        </div>
-       <br/>
+      <div className="my-6 flex items-center gap-2">
+  <label className="relative inline-flex items-center cursor-pointer">
+    <input
+      type="checkbox"
+      onChange={(e) => setPublish(e.target.checked)}
+      checked={publish}
+      className="sr-only peer"
+    />
+    {/* Track */}
+    <div className="w-10 h-5 bg-slate-300 rounded-full peer-checked:bg-green-500 transition"></div>
+    {/* Knob (needs to be inside the same label for peer to work) */}
+    <span className="absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition peer-checked:translate-x-5"></span>
+  </label>
+  <p className="text-sm">Make this image Public</p>
+</div>
+
+       
        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
         <Hash className='w-5 '/>
         Generate Title
@@ -42,7 +58,7 @@ function GenerateImage() {
         </div>
        <div className='flex-1 flex justify-center items-center'>
         <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
-          <Hash className='w-9 h-9' />
+          <Image className='w-9 h-9' />
           <p>Enter a topic and click Generate Title to get started</p>
         </div>
 
